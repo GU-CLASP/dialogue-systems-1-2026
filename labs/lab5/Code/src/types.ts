559f440e3928239c5611e9ddbde8ce163cd79e6f
Linnea Caj <nea.cajuste@gmail.com>
2026-03-26 17:00:20 +0100

    add deployment details + fix time parsing

--- labs/lab5/Code/src/types.ts
+++ labs/lab5/Code/src/types.ts
@@ -4,10 +4,11 @@ import type { ActorRef } from "xstate";
 export interface DMContext {
   spstRef: ActorRef<any, any>;
   lastResult: Hypothesis[] | null;
-  person?: string;
-  day?: string;
-  time?: string;
-  wholeDay?: boolean;
+  person?: string | null;
+  day?: string | null;
+  time?: string | null;
+  wholeDay?: boolean | null;
+  meetingTime?: string | null;
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" } | { type: "DONE" };
